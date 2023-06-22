2a93fdc98b55d43b276dc1f3265b9bf2112c5443
MeZa <80172943+MeZe2110@users.noreply.github.com>
2023-06-22 10:39:18 +0100

    22/06/2023

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -6,10 +6,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {MatCheckboxModule} from '@angular/material/checkbox'; 
-import {MatRadioModule} from '@angular/material/radio'; 
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'; 
-import {MatSelectModule} from '@angular/material/select'; 
+import { MatCheckboxModule } from '@angular/material/checkbox'; 
+import { MatRadioModule } from '@angular/material/radio'; 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'; 
+import { MatSelectModule } from '@angular/material/select'; 
+import { MatSidenavModule } from '@angular/material/sidenav'; 
 
 @NgModule({
   exports: [
@@ -23,7 +24,8 @@ import {MatSelectModule} from '@angular/material/select';
     MatCheckboxModule,
     MatRadioModule,
     MatProgressSpinnerModule,
-    MatSelectModule
+    MatSelectModule,
+    MatSidenavModule
   ]
 })
 export class MaterialModule {}
